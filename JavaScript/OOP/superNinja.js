3c7afe9c5db989b7b00531489f117a3dd837610f
Dan <e.danik90@gmail.com>
2020-03-03 10:38:11 -0600

    added SuperNinja assignment

--- JavaScript/OOP/superNinja.js
+++ JavaScript/OOP/superNinja.js
@@ -17,7 +17,20 @@ class Ninja{
     }
 }
 
-const ninja1 = new Ninja("Hanzo", 50);
-// ninja1.sayName();
-ninja1.showStats();
-ninja1.drinkSake();
+class Sensei extends Ninja{
+    constructor(name, health){
+        super(name, 200)
+        this.wisdom = 10;
+        this.strength = 10;
+        this.speed = 10;
+    }
+
+    speakWisdom(){
+        super.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months");
+    }
+}
+
+const sensei = new Sensei("Kagawa");
+sensei.speakWisdom();
+sensei.showStats();
