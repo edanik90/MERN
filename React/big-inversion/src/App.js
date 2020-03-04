659c1f60bd5c888882284ed21a46b10849936d55
Dan <e.danik90@gmail.com>
2020-03-04 16:05:46 -0600

    added Big Inversion assignment

---
+++ React/big-inversion/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PersonCard from './components/PersonCard'
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard firstName="John" lastName="Smith" age={8} hairColor={"Brown"}/> 
+    </div>
+  );
+}
+
+export default App;
---
+++ React/big-inversion/src/components/PersonCard.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+const PersonCard = ({firstName, lastName, age, hairColor}) => {
+    return (
+        <div>
+            <h1>{lastName}, {firstName}</h1>
+            <p>Age: {age}</p>
+            <p>Hair Color: {hairColor}</p>
+        </div>
+    )
+}
+
+export default PersonCard;
