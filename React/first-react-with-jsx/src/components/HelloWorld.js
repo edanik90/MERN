846949ca3d7a14668d11ecf8404fc1ae11bd536e
Dan <e.danik90@gmail.com>
2020-03-04 10:55:10 -0600

    added FirstReactWithJSX assignment

---
+++ React/first-react-with-jsx/src/components/HelloWorld.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class HelloWorldComponent extends Component {
+    render() {
+        return <h1>Hellow World</h1>
+    }
+}
+
+export default HelloWorldComponent;
