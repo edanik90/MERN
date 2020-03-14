17859dec3695a5f729315d1ace2296d8577ac6eb
Dan <e.danik90@gmail.com>
2020-03-13 22:03:06 -0500

    updated Product Manager assignment (part III)

--- FullStack/ProductManager/client/src/views/Main.js
+++ FullStack/ProductManager/client/src/views/Main.js
@@ -13,7 +13,7 @@ const Main = () => {
                 setLoaded(true);
             })
             .catch(err => console.log("Error: ", err))
-    }, []);
+    }, [products]);
 
     const removeFromDom = productId => {
         setProducts(products.filter(product => product._id !== productId))
